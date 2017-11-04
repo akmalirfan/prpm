47f3dc3d10cf65e5ba41015bf143b9af2469c1fa
akmalirfan <foxkid1994@gmail.com>
2017-11-04 15:38:57 +0800

    Update regex to capture sebutan

--- buang.js
+++ buang.js
@@ -10,15 +10,16 @@ for (word of element.children[0].children[0].children) {
 // Makna dan sumber
 let i = 0
 for (word of element.children[1].children[0].children) {
-    let padanan = word.textContent.match(/Definisi : (.+)\((.+)\)$/i)
-    let makna = padanan[1].trim()
-    let sumber = padanan[2]
-
-    console.log(sumber)
+    let padanan = word.textContent.match(/(\[.*\])?.*Definisi : (.+)\((.+)\)$/i)
+    let sebutan = padanan[1]
+    let makna = padanan[2].trim()
+    let sumber = padanan[3]
 
     if (sumber != 'Kamus Pelajar Edisi Kedua') {
-        meanings[woi[i]] = makna
-        console.log('pushed: ' + makna)
+        meanings[woi[i]] = {
+            sebutan,
+            makna
+        }
     } else {
         // Overwrite the value without removing the allocated space
         // 0 is a random value. It can be anything but a valid word
@@ -33,8 +34,14 @@ woi.sort()
 let maknaperkataan = []
 
 for (let i = 0; i < woi.length; i++) {
+    let kata = {}
+
     if (meanings[woi[i]] != undefined) {
-        maknaperkataan.push(meanings[woi[i]])
+        kata.perkataan = woi[i]
+        kata.sebutan = meanings[woi[i]].sebutan
+        kata.makna = meanings[woi[i]].makna
+
+        maknaperkataan.push(kata)
     }
 }
 
