8af9d44f10cf858303d2b80ee9cbb098d5f8e35a
akmalirfan <foxkid1994@gmail.com>
2017-11-04 20:20:17 +0800

    Disable unnecessary request to PRPM on the landing page

--- index.js
+++ index.js
@@ -8,66 +8,64 @@ app.set('view engine', 'pug')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    JSDOM.fromURL(`http://prpm.dbp.gov.my/Cari1?keyword=${req.query.perkataan}`).then(dom => {
-        let element = dom.window.document.querySelector('.panelDBP')
-        let maknaperkataan = []
+    if (req.query.perkataan) {
+        JSDOM.fromURL(`http://prpm.dbp.gov.my/Cari1?keyword=${req.query.perkataan}`).then(dom => {
+            let element = dom.window.document.querySelector('.panelDBP')
+            let maknaperkataan = []
 
-        if (element != undefined) {
-            let woi = []
-            let meanings = {}
-            
-            // Bilangan makna
-            for (word of element.children[0].children[0].children) {
-                woi.push(word.textContent)
-            }
-            
-            // Makna dan sumber
-            let i = 0
-            for (word of element.children[1].children[0].children) {
-                let padanan = word.textContent.match(/(\[.*\])?.*Definisi : (.+)\((.+)\)$/i)
-                let sebutan = padanan[1]
-                let makna = padanan[2].trim()
-                let sumber = padanan[3]
-            
-                if (sumber != 'Kamus Pelajar Edisi Kedua') {
-                    meanings[woi[i]] = {
-                        sebutan,
-                        makna
+            if (element != undefined) {
+                let woi = []
+                let meanings = {}
+                
+                // Bilangan makna
+                for (word of element.children[0].children[0].children) {
+                    woi.push(word.textContent)
+                }
+                
+                // Makna dan sumber
+                let i = 0
+                for (word of element.children[1].children[0].children) {
+                    let padanan = word.textContent.match(/(\[.*\])?.*Definisi : (.+)\((.+)\)$/i)
+                    let sebutan = padanan[1]
+                    let makna = padanan[2].trim()
+                    let sumber = padanan[3]
+                
+                    if (sumber != 'Kamus Pelajar Edisi Kedua') {
+                        meanings[woi[i]] = {
+                            sebutan,
+                            makna
+                        }
+                    } else {
+                        // Overwrite the value without removing the allocated space
+                        // 0 is a random value. It can be anything but a valid word
+                        woi[i] = 0
                     }
-                } else {
-                    // Overwrite the value without removing the allocated space
-                    // 0 is a random value. It can be anything but a valid word
-                    woi[i] = 0
+                
+                    i++
                 }
-            
-                i++
-            }
-            
-            // Sort
-            woi.sort()
-            
-            for (let i = 0; i < woi.length; i++) {
-                if (meanings[woi[i]] != undefined) {
-                    maknaperkataan.push({
-                        perkataan: woi[i],
-                        sebutan: meanings[woi[i]].sebutan,
-                        makna: meanings[woi[i]].makna
-                    })
+                
+                // Sort
+                woi.sort()
+                
+                for (let i = 0; i < woi.length; i++) {
+                    if (meanings[woi[i]] != undefined) {
+                        maknaperkataan.push({
+                            perkataan: woi[i],
+                            sebutan: meanings[woi[i]].sebutan,
+                            makna: meanings[woi[i]].makna
+                        })
+                    }
                 }
             }
-        }
 
-        res.render(
-            'index',
-            {
-                title: 'Kamus Dewan',
+            res.render('index', {
                 query: req.query.perkataan,
                 maknaperkataan
-            }
-        )
-    })
+            })
+        })
+    } else {
+        res.render('index')
+    }
 })
 
-app.listen(80, () => {
-    console.log('Listening on port 80!')
-})
+app.listen(80, console.log('Listening on port 80!'))
